20e5941f4096b0d1adae06261cf2398244dcad75
kenmpark <102880862+kenmpark@users.noreply.github.com>
2022-04-16 15:35:28 -0500

    first project

---
+++ mycicd/src/main/java/com/itexps/marketing/mycicd/Employee.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itexps.marketing.mycicd;
+
+/**
+ *
+ * @author kenmp
+ */
+public class Employee {
+
+    private int id;
+    private String first;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public Employee(int id, String first) {
+        this.id = id;
+        this.first = first;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "id=" + id + ", first=" + first + '}';
+    }
+
+}
